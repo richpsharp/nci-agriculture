359c26077c79103d505625336634802a60b4dee9
Rich Sharp <richpsharp@gmail.com>
2019-11-21 18:06:10 +0000

    building adjusted crop map

--- fert_cost.py
+++ fert_cost.py
@@ -57,9 +57,17 @@ crop_name_to_mf_cost_map = {
         'monfreda_id': x[1][1],
         'avg_global_price': x[1][2]
         } for x in crop_global_cost_df.iterrows()}
+
+# index by region including
+adjusted_global_price_map = {}
 for row in unique_names.iterrows():
     group_name = row[1][1]
     group_id = row[1][0]
+
+    # China has no local price data, so use Eastern China in that case
+    if group_id == 9999:  # China
+        group_id = 5302  # Eastern Asia
+
     # get the subset of group/item/avgPP for this group
     crop_group = price_per_ton.loc[price_per_ton['group'] == group_id]
     crop_items_in_global_list = [
@@ -78,8 +86,37 @@ for row in unique_names.iterrows():
         for x in crop_items_in_global_list]
     global_avg = numpy.mean(global_cost_list)
     local_avg = numpy.mean(local_cost_list)
-    print(
-        '%s=%.2f of global\n\tglobal: (%.2f)\n\t%s: (%.2f)' %
-        (group_name, global_avg/local_avg, global_avg, group_name, local_avg))
+    price_adjustment_rate = local_avg / global_avg
 
-    price_adjustment_rate = global_avg / local_avg
+    adjusted_global_price_map[group_name] = {}
+    for crop in crop_name_to_mf_cost_map:
+        if crop in crop_group['item'].values:
+            cost = float(crop_group.loc[crop_group['item'] == crop]['avgPP'])
+        else:
+            cost = crop_name_to_mf_cost_map[crop.lower()]['avg_global_price']
+            if isinstance(cost, str):
+                cost = float(cost.replace(',', ''))
+            cost *= price_adjustment_rate
+        adjusted_global_price_map[group_name][crop] = cost
+
+# lastly, stick global prices in there too:
+adjusted_global_price_map['Global'] = {}
+for crop_name in crop_name_to_mf_cost_map:
+    adjusted_global_price_map['Global'][crop_name] = (
+        crop_name_to_mf_cost_map[crop_name]['avg_global_price'])
+
+with open('adjusted_global_price_map.csv', 'w') as adjusted_global_price_file:
+    header_list = ['Global'] + [
+        x for x in sorted(adjusted_global_price_map.keys())
+        if x != 'Global']
+    adjusted_global_price_file.write(
+        ','.join(([''] + header_list)))
+    adjusted_global_price_file.write('\n')
+    for crop_name in sorted(adjusted_global_price_map['Global'].keys()):
+        adjusted_global_price_file.write('"%s",' % crop_name)
+        for region in header_list:
+            price = adjusted_global_price_map[region][crop_name]
+            if isinstance(price, str):
+                price = float(price.replace(',', ''))
+            adjusted_global_price_file.write('%.2f,' % price)
+        adjusted_global_price_file.write('\n')
